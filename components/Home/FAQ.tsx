cb711f9ad65252a7bcf398b4c918b015e5ef2d54
BardNilz <80070896+BardNilz@users.noreply.github.com>
2022-07-07 21:39:18 +0200

    Update FAQ.tsx

--- components/Home/FAQ.tsx
+++ components/Home/FAQ.tsx
@@ -27,49 +27,53 @@ export default function Example() {
         <Accordion>
           <AccordionItem>
             <AccordionItemHeading>
-              <AccordionItemButton>Vad är n0llningen?</AccordionItemButton>
+              <AccordionItemButton>Vad är en n0llning?</AccordionItemButton>
             </AccordionItemHeading>
             <AccordionItemPanel>
               <p>
-                N011ningen är ett namn på de fem førsta veck0rna av er tid här
-                på 1TH. Den førsta av dessa utspe1ar sig innan 0rdinarie
-                undervisning børjar 0ch det är här s0m aktiviteterna 1igger s0m
-                tätast. På dagarna ges nyttiga intr0dukti0nskurser i diverse
-                ämnen 0ch på kvä11arna finns det r01iga fritidsaktiviteter 0ch
-                fester att de1ta i. De f0rsta veck0rna när man k0mmer ti11 ett
-                universitet kanske man känner sig ensam, e11er så 0r0ar man sig
-                0ver 0m man k0mmer träffa några k0mpisar e11er ej. Det är precis
-                detta s0m n011ningen finns här f0r, efters0m det är vä1digt
-                många studenter s0m b0rjar utan att känna en enda pers0n i he1a
-                staden. Av tidigare studenters erfarenheter så träffar man 0fta
-                några av sina närmsta vänner 0ch skapar he1a umgängeskretsar
-                under n011ningen.
+                N0llningen är ett namn på de fem första veckorna av er tid här
+                på LTH. Den första av dessa utspelar sig innan ordinarie
+                undervisning börjar och det är här som aktiviteterna ligger som
+                tätast. På dagarna ges nyttiga introduktionskurser i diverse
+                ämnen och på kvällarna finns det roliga fritidsaktiviteter och
+                fester att delta i. De första veckorna när man kommer till ett
+                universitet kanske man känner sig ensam, eller så oroar man sig
+                over om man kommer träffa några kompisar eller ej. Det är precis
+                detta som n0llningen finns här för, eftersom det är väldigt
+                många studenter som börjar utan att känna en enda person i hela
+                staden. Av tidigare studenters erfarenheter så träffar man ofta
+                några av sina närmsta vänner och skapar hela umgängeskretsar
+                under n0llningen.
               </p>
             </AccordionItemPanel>
           </AccordionItem>
           <AccordionItem>
             <AccordionItemHeading>
-              <AccordionItemButton>Vad är pepparna? </AccordionItemButton>
+              <AccordionItemButton>Vad är en peppare? </AccordionItemButton>
             </AccordionItemHeading>
             <AccordionItemPanel>
               <p>
-                In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
-                nostrud velit in irure cillum tempor laboris sed adipisicing eu
-                esse duis nulla non.
+              En peppare är en person som bistår de som har planerat n0llningen
+              i deras arbete med att planera och utföra n0llningen. De springer
+              allt som oftast runt i rosa frackar och ser till så att både 
+              n0llor och phaddrar är peppade. De vet allt om hur n0llningen 
+              fungerar - om du har missat att köpa biljett till en sittning 
+              eller känner dig osäker på var och när du ska infinna dig till 
+              olika event så kan du alltid prata med en peppare!
               </p>
             </AccordionItemPanel>
           </AccordionItem>
           <AccordionItem>
             <AccordionItemHeading>
-              <AccordionItemButton>Vad är phaddrarna? </AccordionItemButton>
+              <AccordionItemButton>Vad är en phadder? </AccordionItemButton>
             </AccordionItemHeading>
             <AccordionItemPanel>
               <p>
                 En phadder är en äldre student som ofta springer runt i en
                 ouveralle, och en del springer runt utan ouveralle. Phaddern vet
-                en fruktansvärd massa saker om LTH och då speciellt hur
+                en fruktansvärt massa saker om LTH och då speciellt hur
                 D-sektionen och studentlivet fungerar. En phadder kan också allt
-                som står på nollningsprogrammet, var foreläsningssalar ligger,
+                som står på n0llningsprogrammet, var foreläsningssalar ligger,
                 hur man beställer pizza och var man tar vägen om man missade
                 sista tåget hem. Alla phaddrarna har även gått på utbildning i
                 ledarskap, alkoholansvar och mycket, mycket mer, så tveka inte
@@ -84,12 +88,12 @@ export default function Example() {
             </AccordionItemHeading>
             <AccordionItemPanel>
               <p>
-                En N011a är 0ftast en f0rstaårsstudent. Denna student tas hand
-                0m av phaddrarna s0m a11tid ser ti11 att N011an har det
-                trev1igt! S0m N011a får man gå på massvis av r01iga event 0ch
-                fester samt bekanta sig med sina nya kursare. N011a är man fram
-                ti11 n011egasquen (n011ningens avs1utningsfest) då man upph0js
-                ti11 etta efter fu11b0rdat n011euppdrag.
+                En n0lla är oftast en förstaårsstudent. Denna student tas hand
+                om av phaddrarna som a11tid ser till att n0llan har det
+                trevligt! Som n0lla får man gå på massvis av roliga event och
+                fester samt bekanta sig med sina nya kursare. N0lla är man fram
+                till n0llegasquen (n0llningens avslutningsfest) då man upphöjs
+                till etta efter fullbordat n0lleuppdrag.
               </p>
             </AccordionItemPanel>
           </AccordionItem>
